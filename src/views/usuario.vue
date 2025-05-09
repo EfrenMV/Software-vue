4f507dc981be99065647ab8c774daeedfc3ef868
EfrenMV <efrenvillanueva148@gmail.com>
2025-05-09 15:33:57 -0600

    Update && Feak [usuario.vue]
    - Se agregaron mas styles a los input y al fondo del usuario
    
    - Se agrego el btn que indica que es usuario
    - Se agrego la imagen
    - Se agrego el nombre
    - Se agrego el circulo verde

--- src/views/usuario.vue
+++ src/views/usuario.vue
@@ -3,18 +3,26 @@
   <div class="usuario">
   <div class="box">
     <div class="fondoUsuario">
-        <div class="hedear-usuario"></div>
+      <div class="hedear-usuario">
+        <button class="btnPerfil">Mecánico</button>
+        <div class="Camara dos"></div>
+      </div>
+        <div class="box_img">
+        <img src="../components/icons/userProfile.png" alt="perfil" />
+        <p style="color: aliceblue;">Juan Mecanico</p>
+      </div>
+
     </div>
     <div class="input-container">
-      <p>Name</p>
+      <p class="tx1">Name</p>
       <input type="text" placeholder=""/>
     </div>
 
     <div class="input-container">
-    <p>Password</p>
+    <p class="tx">Password</p>
     <input type="password" placeholder="" />
     </div>
-    <button>Save changes</button>
+    <button class="btnSave">Save changes</button>
   </div>  
     
   <Menu></Menu>    
@@ -49,7 +57,8 @@ import Header from '@/components/Header.vue'
     justify-content: flex-start;
     background-color: #0B0C1A;
     width: 100%;
-    height: 40%;
+    height: 44%;
+    border-radius: 0px 0px 30px 30px;
   }
 
 
@@ -57,9 +66,9 @@ import Header from '@/components/Header.vue'
     display: flex;
     justify-content: space-around;
     align-items: center;
-    background-color: aqua;
     width: 100%;
     height: 20%;
+    gap: 100px;
   }
 
   .input-container{
@@ -69,8 +78,27 @@ import Header from '@/components/Header.vue'
     justify-content: center;
     width: 100%;
   }
- 
-  button{
+  
+  .input-container input{
+     margin-top: -10px;
+     padding: 10px 50px;
+     border-radius: 5px;
+     border-color: #544c4c2a;
+   }
+
+   .input-container .tx{
+     margin-right: 200px;
+    }
+    
+   .input-container .tx1{
+     margin-right: 220px;
+   }
+
+   .input-container .tx, .tx1{
+    font-weight: bold;
+    }
+
+  .btnSave{
     padding: 10px 50px;
     margin: 60px 0;
     color: white;
@@ -78,5 +106,36 @@ import Header from '@/components/Header.vue'
     background-color: #0B0C1A;
     border-radius: 30px;
   }
+
+  .btnPerfil{
+    padding: 8px 40px;
+    margin: 60px 0;
+    border-radius: 30px;
+  }
+
+  /* Optenida del Header.vue*/
+  .dos{
+    background-color: #3BD135;
+    width: 20px;
+    height: 20px;
+  }
+
+    /* Optenida del Header.vue*/
+  .box_img{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+  }
+
+  .box_img p{
+   font-family: "Open Sans";
+   font-size: 20px;
+   font-weight: bold;
+
+  }
+
+
+ 
 
 </style>
