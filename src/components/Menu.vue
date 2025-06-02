b3476e7f7123d8fa50f7614f095090e00b6837cd
EfrenMV <efrenvillanueva148@gmail.com>
2025-06-01 18:46:51 -0600

    Update [login.vue]
    - Se agrego el colorcito de fondo

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,20 +2,20 @@
     <div class="contenedor">
       <div class="Menu">
         <div class="icon-container">
-          <!-- Carro -->
-          <router-link to="/vehiculos">
+          <!-- Carro -->     
+          <router-link to="/vehiculos" class="icon-link" :class="{ active: $route.path === '/vehiculos' }">
           <i class="fas fa-car icon"></i>
-          </router-link>
+        </router-link>
 
-          <!-- Llave inglesa -->
-           <RouterLink to="/menu-nerudo">
+        <!-- Llave inglesa -->
+        <RouterLink to="/menu-nerudo" class="icon-link" :class="{ active: $route.path === '/menu-nerudo' }">
           <i class="fas fa-wrench icon"></i>
-          </RouterLink>
-  
-          <!-- Usuario -->
-          <router-link to="/usuario">
+        </RouterLink>
+
+        <!-- Usuario -->
+        <router-link to="/usuario" class="icon-link" :class="{ active: $route.path === '/usuario' }">
           <i class="fas fa-user icon"></i>
-          </router-link>
+        </router-link>
 
         </div>
       </div>
@@ -45,31 +45,46 @@
     background-color: black;
   }
   
-  .icon-container {
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
-    padding: 0 20px;
-  }
-  
-  .icon {
-    font-size: 20px;
-    color: white;
-    cursor: pointer;
-   }
-  
-  .diagonal {
-    height: 4px;
-    width: 90px;
-    background-color: rgb(6, 6, 6);
-    border-radius: 2px;
-  }
-  
-  .cDiagonal {
-    margin-top: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.icon-container {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  padding: 0 20px;
+}
+
+.icon-link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  transition: background-color 0.3s ease;
+  text-decoration: none;
+}
+
+.icon-link.active {
+  background-color: #4a5568; /* Color gris para el estado activo */
+}
+
+.icon {
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+}
+
+.diagonal {
+  height: 4px;
+  width: 90px;
+  background-color: rgb(6, 6, 6);
+  border-radius: 2px;
+}
+
+.cDiagonal {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
   </style>
   
